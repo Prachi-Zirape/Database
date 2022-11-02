e7cc148266030973d0fe297d4e22b5bb71ef7518
Prachi Harley <przirape7@gmail.com>
2022-11-02 19:00:34 -0400

    Update SQLQuery_2.sql

--- W3School/SQL_JOINs/Customer_Database/SQLQuery_2.sql
+++ W3School/SQL_JOINs/Customer_Database/SQLQuery_2.sql
@@ -1,3 +1,5 @@
+--write a SQL query to find those orders where the order amount exists between 500 and 2000. Return ord_no, purch_amt, cust_name, city.
+
 select a.ord_no,a.purch_amt, b.cust_name, b.city from orders a inner join customer b
 on a.salesman_id=b.salesman_id
 where a.purch_amt between 500 and 2000
